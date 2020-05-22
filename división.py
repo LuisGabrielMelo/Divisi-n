720ee284e397ecf56798b852acdd6f8fe5ceaf9b
reggae93 <35476328+reggae93@users.noreply.github.com>
2020-05-22 00:01:25 -0300

    Add files via upload

---
+++ división.py
@@ -0,0 +1,10 @@
+print("Ingreso De dos Numeros y su división")
+n1=int(input("Ingrese el Primer Numero: "))
+n2=int(input("Ingrese el Segundo Numero: "))
+
+división = n1/n2;
+
+print("La división es : " + str(división))
+
+print("Felicidades la división de los dos numeros fue correcta :D")
+
